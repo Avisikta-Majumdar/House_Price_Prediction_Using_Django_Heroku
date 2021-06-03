f74cd4a625c77e915f052ae8b0bd014d6cb64089
Avisikta Majumdar <47344480+Avisikta-Majumdar@users.noreply.github.com>
2021-06-03 19:37:31 +0530

    Add files via upload

---
+++ hpp/frontp/Add This Code To Your Django Code.py
@@ -0,0 +1,8 @@
+import PredictionPart
+print(round(PredictionPart.predict_Bangalore_house_price("Attibele",250,3)))
+
+#Have to do
+"""
+If user select city as kolkata then it will use predict_Kolkata_house_price method -->code akash
+
+"""
---
+++ hpp/frontp/AllCityColumnName.py
@@ -0,0 +1,11 @@
+#This .py file will contain only the columns name of each city
+
+kolkata=["Area", "No. Of Bedrooms", "Alipore", "Ballygunge", "Bansdroni", "Barisha Purba Para Road", "Behala",
+  "Dum Dum", "Garia", "Howrah", "Kasba", "Keshtopur", "Konnagar", "Madhyamgram", "Madurdaha Hussainpur", "Mukundapur",
+  "Narendrapur", "Nayabad", "New Alipore",
+  "New Town", "Rajarhat", "Salt Lake City", "Sodepur", "Sonarpur", "Tangra", "Tiljala", "Tollygunge", "Uttarpara Kotrung"]
+
+mumbai = ["Area", "No. Of Bedrooms", "Andheri East", "Andheri West", "Boisar", "Borivali West", "Chembur", "Dahisar", "Dombivali", "Goregaon East", "Goregaon West", "Kalyan West", "Kamothe", "Kandivali East", "Kandivali West", "Kharghar", "Magathane", "Malad East", "Malad West", "Mira Road East", "Mulund West",
+                  "Naigaon East", "Nala Sopara", "Panvel", "Powai", "Taloja", "Thane", "Thane West", "Ulwe", "Ville Parle East", "Virar"]
+
+
